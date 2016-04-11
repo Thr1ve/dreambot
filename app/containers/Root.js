b0f1405e51cea92950f7142e93d7536b878c2d2e
Thr1ve <fict1on.buhler@gmail.com>
2016-04-10 21:24:56 -0400

    (refactor) use one Root file

--- app/containers/Root.js
+++ app/containers/Root.js
@@ -1,7 +1,25 @@
-// From: https://github.com/reactjs/redux/blob/master/examples/real-world/containers/Root.js
+// From: https://github.com/reactjs/redux/blob/master/examples/real-world/containers/Root.dev.js
 
-if (__DEV__ === 'production') {
-  module.exports = require('./Root.prod');
-} else {
-  module.exports = require('./Root.dev');
-}
+import React, { PropTypes } from 'react';
+import routes from '../config/routes';
+import { Router } from 'react-router';
+import { Provider } from 'react-redux';
+
+const Root = React.createClass({
+  propTypes: {
+    store: PropTypes.object.isRequired,
+    history: PropTypes.object.isRequired
+  },
+
+  // TODO: Wrap Auth around this render function?
+  render() {
+    const { store, history } = this.props;
+    return (
+      <Provider store={store}>
+        <Router history={history} routes={routes} />
+      </Provider>
+    );
+  }
+});
+
+export default Root;
