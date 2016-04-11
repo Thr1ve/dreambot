4cbaf7dc572d92342edc1eedf2c48ffad6ed761c
Thr1ve <fict1on.buhler@gmail.com>
2016-04-10 22:02:44 -0400

    (refactor) wipe actions and reducers

--- app/reducers/index.js
+++ app/reducers/index.js
@@ -1,27 +1,11 @@
 import { combineReducers } from 'redux';
 import { routerReducer as routing } from 'react-router-redux';
-import messages from './messagesReducer';
-import searchBox from './SearchBoxReducer';
-import averages from './averagesReducer';
 import notifications from './notificationsReducer';
-import wordCount from './wordCountReducer';
-import classify from './questionReducer';
-import engagement from './engagementReducer';
-import profile from './userProfileReducer';
-import cohortProfiles from './cohortProfilesReducer';
 import auth from './authReducer';
 
 const rootReducer = combineReducers({
   routing,
-  messages,
-  searchBox,
-  averages,
   notifications,
-  wordCount,
-  classify,
-  engagement,
-  profile,
-  cohortProfiles,
   auth
 });
 
