ba24a0d5a133921b1dec059ada0868a421e08035
Thr1ve <fict1on.buhler@gmail.com>
2016-04-10 22:16:21 -0400

    (refactor) wipe styles

--- app/styles/index.js
+++ app/styles/index.js
@@ -1,34 +1,5 @@
 const styles = {
-  title: {
-    marginLeft: '40%',
-    color: 'darkblue'
-  },
-  space: {
-    marginTop: '25px'
-  },
-  graph: {
-    // backgroundColor: 'rgba(245,245,245,1)'
-  },
-  messages: {
-    width: '500px',
-    marginLeft: '20%',
-    textAlign: 'center'
-  },
-  user: {
-    color: 'blue'
-  },
-  ofprot: {
-    height: '750px',
-    overflow: 'scroll'
-  },
-  cards:{
-    margin: '8px',
-    'borderRadius': '2px',
-    width: 'auto'
-  },
-  userInfo:{
-    'wordWrap': 'break-word'
-  }
+
 };
 
 export default styles;
