b0424775ca559e077cbefaa161a7868d32739b91
Thr1ve <fict1on.buhler@gmail.com>
2016-04-10 22:52:39 -0400

    (refactor) make login a functional component

--- app/components/Login.js
+++ app/components/Login.js
@@ -1,13 +1,9 @@
 import React from 'react';
 
-const Login = React.createClass({
-  render() {
-    return (
-      <div>
-        <a href="/auth/github" className="button"> Login with Github </a>
-      </div>
-    )
-  }
-})
+const Login = () => (
+  <div>
+    <a href="/auth/github" className="button"> Login with Github </a>
+  </div>
+);
 
 export default Login;
