c614d520b56c2f080cc35df9f609417727ff3dc6
Thr1ve <fict1on.buhler@gmail.com>
2016-04-10 22:52:53 -0400

    (refactor) set up home for query testing

--- app/containers/Home.js
+++ app/containers/Home.js
@@ -1,8 +1,37 @@
+/* eslint-disable no-unused-vars */
+
 import React from 'react';
 import { connect } from 'react-redux';
 
+import {
+  getMessages,
+  getSearchResults, // word:string
+  getAvgMessagesByHour, // { year, month, day }
+  getVolumeOfMessagesByHour, // { year, month, day }
+  getAvgMessagesByDayOfWeek,
+  getAllUniqueWords,
+  queryWordCountByUser,
+  queryUserMessagesById,
+  getClassifications,
+  getUserMessageReduction,
+  getSingleUserMessageReduction, // userId:string
+ } from '../actions/queries';
+
 const Home = React.createClass({
   componentWillMount() {
+    // getSingleUserMessageReduction('U0S1U1H54')
+    // getUserMessageReduction()
+    // getClassifications()
+    // queryUserMessagesById()
+    // queryWordCountByUser()
+    // getAvgMessagesByDayOfWeek()
+    // getAllUniqueWords()
+    // getAvgMessagesByHour({ year: 2016, month: 3, day: 17 })
+    // getVolumeOfMessagesByHour({ year: 2016, month: 3, day: 17 })
+    // getSearchResults('love')
+    getMessages()
+      .then(res => console.log(res))
+      .catch(err => console.log(err));
   },
 
   render() {
