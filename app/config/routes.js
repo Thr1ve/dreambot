967077b5084e9c3f6f6b8f5150b5ef017b7f3599
Thr1ve <fict1on.buhler@gmail.com>
2016-04-10 21:25:56 -0400

    (refactor) wipe routes

--- app/config/routes.js
+++ app/config/routes.js
@@ -4,12 +4,8 @@ import { Route, IndexRoute, IndexRedirect } from 'react-router';
 import Main from '../components/Main';
 import App from '../containers/App';
 import Home from '../components/Home';
-import Questions from '../components/Questions';
-import AveragesContainer from '../containers/AveragesContainer';
-import UserProfileContainer from '../containers/UserProfileContainer';
 import LoginContainer from '../containers/LoginContainer';
 import CatchContainer from '../containers/CatchContainer';
-import CohortContainer from '../containers/CohortContainer';
 
 const Routes = (
   <Route path="/" component={Main}>
@@ -18,10 +14,6 @@ const Routes = (
     <Route path="/catch" component={CatchContainer} />
     <Route path="/app" component={App} >
       <IndexRoute component={Home} />
-      <Route path="/user/:id" component={UserProfileContainer} />
-      <Route path="/questions" component={Questions} />
-      <Route path="/averages" component={AveragesContainer} />
-      <Route path="/cohort" component={CohortContainer} />
     </Route>
   </Route>
 );
