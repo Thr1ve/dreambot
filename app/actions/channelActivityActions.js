901b6f4611b1dfadfc9be9114bda99926cf0f7d8
Thr1ve <fict1on.buhler@gmail.com>
2016-04-15 13:27:05 -0400

    (chore) refactor actions organization and add comments

--- app/actions/channelActivityActions.js
+++ app/actions/channelActivityActions.js
@@ -5,14 +5,7 @@ import {
   getDelimiter, isValidDate, buildDatesArray
 } from '../utils/time';
 
-export const SET_DATES = 'SET_DATES';
-export const setDates = dates =>
-  ({ type: SET_DATES, dates });
-
-export const SET_DATE_RANGE = 'SET_DATE_RANGE';
-export const setDateRange = (dateRange) =>
-  ({ type: SET_DATE_RANGE, dateRange });
-
+// VOLUMES:
 export const REQUEST_VOLUMES = 'REQUEST_VOLUMES';
 export const requestVolumes = (dateKey, delimiter) =>
   ({ type: REQUEST_VOLUMES, dateKey, delimiter });
@@ -21,16 +14,11 @@ export const RECEIVE_VOLUMES = 'RECEIVE_VOLUMES';
 export const receiveVolumes = (data, dateKey, delimiter) =>
   ({ type: RECEIVE_VOLUMES, data, dateKey, delimiter });
 
-export const SET_DELIMITER = 'SET_DELIMITER';
-export const setDelimiter = delimiter =>
-  ({ type: SET_DELIMITER, delimiter });
-
-function shouldFetchVolumes(state, date, delimiter) {
-  const { volumes } = state.channelActivity;
-  const dateKey = getDateAsKey(date, delimiter);
-
-  return !volumes[delimiter][dateKey];
-}
+// If we want to handle invalidating our data, we would
+// expand this function. However, as the data for a past
+// date should never change, we're opting not to do that.
+const shouldFetchVolumes = (state, dateKey, delimiter) =>
+  !state.channelActivity.volumes[delimiter][dateKey];
 
 const fetchVolumes = (date, delimiter) => dispatch => {
   const dateKey = getDateAsKey(date);
@@ -53,10 +41,16 @@ export const fetchVolumesIfNeeded = date => (dispatch, getState) => {
     return console.error('Invalid Date', date);
   }
   const delimiter = getDelimiter(date);
-  if (shouldFetchVolumes(getState(), date, delimiter)) {
+  if (shouldFetchVolumes(getState(), getDateAsKey(date, delimiter), delimiter)) {
     return dispatch(fetchVolumes(date, delimiter));
   }
 };
+
+
+// DATES
+export const SET_DATES = 'SET_DATES';
+export const setDates = dates =>
+  ({ type: SET_DATES, dates });
 
 export const fetchOrSetDates = (datesArray) => dispatch => {
   datesArray.forEach((dateKey) => {
@@ -65,18 +59,16 @@ export const fetchOrSetDates = (datesArray) => dispatch => {
   return dispatch(setDates(datesArray));
 };
 
-export const updateDelimiter = delimiter => (dispatch, getState) => {
-  const { currentDateRange } = getState().channelActivity;
-  const newDatesArray = buildDatesArray(currentDateRange, delimiter);
-  dispatch(setDelimiter(delimiter));
-  dispatch(fetchOrSetDates(newDatesArray));
-};
-
 const updateDates = dateRange => (dispatch, getState) => {
   const { delimiter } = getState().channelActivity;
   const newDatesArray = buildDatesArray(dateRange, delimiter);
   return dispatch(fetchOrSetDates(newDatesArray));
 };
+
+// DATE RANGE
+export const SET_DATE_RANGE = 'SET_DATE_RANGE';
+export const setDateRange = (dateRange) =>
+  ({ type: SET_DATE_RANGE, dateRange });
 
 // Should we just have the dateRange hold its own delimiter and do the
 // error handling elsewhere?
@@ -88,3 +80,14 @@ export const safeSetDateRange = dateRange => dispatch => {
   }
 };
 
+// DELIMITER
+export const SET_DELIMITER = 'SET_DELIMITER';
+export const setDelimiter = delimiter =>
+  ({ type: SET_DELIMITER, delimiter });
+
+export const updateDelimiter = delimiter => (dispatch, getState) => {
+  const { currentDateRange } = getState().channelActivity;
+  const newDatesArray = buildDatesArray(currentDateRange, delimiter);
+  dispatch(setDelimiter(delimiter));
+  dispatch(fetchOrSetDates(newDatesArray));
+};
